f3d4924e05442cd8c560eb5fa5a2e2425a2c5b04
jacksonhh <jacksonh_pzo@hotmail.com>
2015-04-11 17:28:43 -0300

    classe Variavel

---
+++ fonte/Variavel.java
@@ -0,0 +1,25 @@
+/**
+ * Class Variavel
+ */
+public class Variavel {
+
+	//
+	//	Atributos
+	//
+	private String nome;
+	private double valor;
+	
+	//
+	// 	Métodos
+	//
+	public void setNome (String n) this.nome = n;
+	public String getNome () return this.nome;
+
+	public void setValor (double d) this.valor = d;
+	public double getValor () return this.valor;
+	
+	
+	
+	
+	
+}
