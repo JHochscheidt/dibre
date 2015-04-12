507897cdff239ada683d6c16c736dd48f0ff6e6e
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-12 00:14:14 -0300

    classe Escopo

---
+++ fonte/Escopo.java
@@ -0,0 +1,22 @@
+class Escopo{
+
+	short inicio;
+	short fim;
+
+
+	public void setInicio(short i){
+		this.inicio = i;
+	}
+	public short getInicio(){
+		return this.inicio;
+	}
+	
+	public setFim(short f){
+		this.fim = f;
+	}
+	public short getFim(){
+		return this.fim;
+	}
+	
+	
+}
