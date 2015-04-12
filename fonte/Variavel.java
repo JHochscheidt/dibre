8cad072c06622a074af57cda7c0fae8a74621f95
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-12 02:25:28 -0300

    nao funciona, apenas fazendo testes de strings

--- fonte/Variavel.java
+++ fonte/Variavel.java
@@ -17,6 +17,22 @@ public class Variavel {
 
 	public void setValor (double d) { this.valor = d; }
 	public double getValor () { return this.valor; }
-
 	
+	
+	public void validarDeclaracao(String[] tokens){
+		
+		if(tokens[0] == null || tokens[0].trim().length() == 0){
+			//validou parte anterior ao token DOUBLE - palavra reservada - 
+			System.out.println("STRING vazia");
+			tokens = tokens[1].split(" ");
+			for(int cont = 0; cont < tokens.length; cont++){
+				if(tokens[cont] == null || tokens[cont].trim().length() == 0){
+					System.out.println(cont + "token vazio");
+				}else{
+					
+				}
+				System.out.println(cont + "'" + tokens[cont] + "'");
+			}
+		}
+	}
 }
