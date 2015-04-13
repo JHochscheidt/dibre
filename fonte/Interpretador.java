f38028d37fdff7b7900e2d5590fbb3e620a9580f
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-13 00:56:08 -0300

    declaracao de variavel sem atribuicao funcionando

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -1,3 +1,4 @@
+
 /* Código copiado dos arquivos disponibilizados pelo professor via moodle
  * 
  * Este código faz a leitura do arquivo fonte a ser interpretado,
@@ -5,38 +6,54 @@
  * 
  * 
  */ 
+
+
 import java.util.ArrayList;
+import java.util.regex.*;
+
 class Interpretador {
     public String linhas[];
-	ArrayList<Variavel> variavel = new ArrayList<Variavel>();
-		
+	public ArrayList<Variavel> variavel = new ArrayList<Variavel>();
+    // string que define com expressão regular, como se dará a declaração de uma variável
+    String declaracaoDeVariavel = "\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,};\\s{0,}";
+    String declaracaoDeVariavelComAtribuicao = 
+    	"\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.?\\d{0,}\\s{0,};\\s{0,}";
+    
+    // metodo que recebe as linhas do arquivo
     public void interpreta(String l[]) {
         this.linhas = l;
+        //percorre as linhas do arquivo
         for(int i = 0; i < this.linhas.length; i++) {
+        	//verifica se a linha está vazia
             if(this.linhas[i] != null) {
                 // TODO: interpretar a linha
-                System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
+                //System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
+                //chama metodo que interpreta cada linha em especifico
                 this.interpretaLinha(this.linhas[i]);
 			}
         }   
     }
+
     
     // método que interpreta uma linha específica
+    // todas as linhas serão interpretadas através de expressão regular
     public void interpretaLinha(String linha){
-		String tokens[];
-		tokens = linha.split("double");
-		
-		// garante que tenha somente 2 strings na linha ???????????????????
-		if(tokens.length == 2){
-			System.out.println("DECLARAÇÃO");
-			Variavel newVar = new Variavel();
-			newVar.validarDeclaracao(tokens);
-			//fazer metodo que verifica se realmente é uma declaracao e se está correta			
-		}
-		
-		
+    	boolean b = linha.matches(declaracaoDeVariavel);
+    	//System.out.println(b);
+    	if(linha.matches(declaracaoDeVariavel)){
+    		//identificar qual o nome da variavel e depois chamar seu metodo construtor
+    		Variavel newVar = new Variavel();
+    		//System.out.println("chama metodo construtor de uma variavel");
+    		newVar.declararVariavel(linha);
+    		variavel.add(newVar);
+    		//System.out.println("'" + variavel.get(0).getNome() + "'");	//imprime o nome da variavel
+    	/*}else if(linha.matches(declaracaoDeVariavelComAtribuicao)){
+    		//identificar qual o nome da variavel e depois chamar seu metodo construtor
+    		System.out.println("chama metodo construtor de uma variavel com atribuicao");
+    	*/
+    	}else{
+    		System.out.println("nao esta entrando nos ifelseifs");
+    	}		
 	}    
-    
-    
     
 }
