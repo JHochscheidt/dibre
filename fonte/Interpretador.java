5c2ea5db99c0eca9316b50dcd97349433766cdfe
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-15 16:46:45 -0300

    correcoes para funcionando da declararVariavelComAtribuicao

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -13,11 +13,21 @@ import java.util.regex.*;
 
 class Interpretador {
     public String linhas[];
+    public static final String espacoEmBranco = "[ \t\nxB\f\r]";
 	public ArrayList<Variavel> variavel = new ArrayList<Variavel>();
     // string que define com expressão regular, como se dará a declaração de uma variável
-    String declaracaoDeVariavel = "\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,};\\s{0,}";
-    String declaracaoDeVariavelComAtribuicao = 
-    	"\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.?\\d{0,}\\s{0,};\\s{0,}";
+    String declaracaoDeVariavel = "\\s{0,}double\\s{1,}[[a-zA-Z]\\w{0,}\\s{0,}]{1,}\\s{0,};\\s{0,}";
+    String declaracaoDeVariavelComAtribuicao = "\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.?\\d{0,}\\s{0,};\\s{0,}";
+    
+	   
+    
+    
+    
+    //"\\s{0,}double\\s{0,}[[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}[0-9]{1,}.{0,1}[0-9]{0,}\\s{0,};\\s{0,}";
+    		
+    		//"\\s{0,}double\\s{1,}[[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.*\\d{0,}\\s{0,}]{1,}\\s{0,};\\s{0,}";
+    		
+    		
     
     // metodo que recebe as linhas do arquivo
     public void interpreta(String l[]) {
@@ -38,22 +48,31 @@ class Interpretador {
     // método que interpreta uma linha específica
     // todas as linhas serão interpretadas através de expressão regular
     public void interpretaLinha(String linha){
-    	boolean b = linha.matches(declaracaoDeVariavel);
-    	//System.out.println(b);
-    	if(linha.matches(declaracaoDeVariavel)){
-    		//identificar qual o nome da variavel e depois chamar seu metodo construtor
-    		Variavel newVar = new Variavel();
-    		//System.out.println("chama metodo construtor de uma variavel");
-    		newVar.declararVariavel(linha);
-    		variavel.add(newVar);
+    	//boolean b = linha.matches(); //System.out.println(b);
+    
+    	if(linha.matches(declaracaoDeVariavel)){ // caso seja somente a declaração da variavel
+    		Variavel newVar = new Variavel(); //System.out.println("chama metodo construtor de uma variavel");
+    		newVar.declararVariavel(linha, variavel);
     		//System.out.println("'" + variavel.get(0).getNome() + "'");	//imprime o nome da variavel
-    	/*}else if(linha.matches(declaracaoDeVariavelComAtribuicao)){
-    		//identificar qual o nome da variavel e depois chamar seu metodo construtor
+    	}else if(linha.matches(declaracaoDeVariavelComAtribuicao)){ // caso seja declaracao com atribuicao de variavel
     		System.out.println("chama metodo construtor de uma variavel com atribuicao");
-    	*/
+    		Variavel newVar = new Variavel();
+    		newVar.declararVariavelComAtribuicao( linha, variavel);    		
     	}else{
     		System.out.println("nao esta entrando nos ifelseifs");
     	}		
 	}    
+    
+    
+    public void imprimeVariaveis(){
+    	for(int indice = 0; indice < this.variavel.size(); indice++){
+        	System.out.println("variavel     " + this.variavel.get(indice).getNome());
+        	System.out.println("varlor       " + this.variavel.get(indice).getValor());
+        }
+    }
+    
+    
+    
+    
     
 }
