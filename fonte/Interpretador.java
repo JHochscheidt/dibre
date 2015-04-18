742fe0ac08f8242f3f93850c6a15badaf723427a
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-18 17:09:34 -0300

    declaracao de variavel sem atribuicao funcionando, nao precisa mais mexer

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -11,90 +11,107 @@ import java.util.ArrayList;
 import java.util.regex.*;
 
 class Interpretador {
-    public String linhas[];
+    private ArrayList<String> linhas;
     public static final String espacoEmBranco = "[ \t\nxB\f\r]";
     
 	public ArrayList<Variavel> variavel = new ArrayList<Variavel>();
     // string que define com expressão regular, como se dará a declaração de uma variável
-    String declaracaoDeVariavel = "\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,}\\s{0,};\\s{0,}";
-    String declaracaoDeVariavelComAtribuicao = "\\s{0,}double\\s{1,}[a-zA-Z]\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.?\\d{0,}\\s{0,};\\s{0,}";
+    String declaracaoDeVariavel = "\\s{0,}varDouble\\s{1,}[a-zA-Z]{1,}\\w{0,}\\s{0,}\\s{0,};\\s{0,}";
+    String declaracaoDeVariavelComAtribuicao = "\\s{0,}varDouble\\s{1,}[a-zA-Z]{1,}\\w{0,}\\s{0,}=\\s{0,}\\d{1,}\\.?\\d{0,}\\s{0,};\\s{0,}";
         
     // metodo que recebe as linhas do arquivo
-    public void interpreta(String l[]) {
+    public void interpreta(ArrayList<String> l) {
         this.linhas = l;
         //percorre as linhas do arquivo
-        for(int i = 0; i < this.linhas.length; i++) {
+        for(int i = 0; i < this.linhas.size(); i++) {
         	//verifica se a linha está vazia
-            if(this.linhas[i] != null) {
+            if(this.linhas.get(i) != null) {
                 // TODO: interpretar a linha
                 //System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
                 //chama metodo que interpreta cada linha em especifico
-                this.interpretaLinha(this.linhas[i]);
+                this.interpretaLinha(this.linhas.get(i), i);
 			}
         }   
     }
-
     
     // método que interpreta uma linha específica
     // todas as linhas serão interpretadas através de expressão regular
-    public void interpretaLinha(String linha){
+    public void interpretaLinha(String linha, int i){
     	//boolean b = linha.matches(); //System.out.println(b);
-    	ArrayList<String> tokens = new ArrayList<String>(); 
-    	tokens.addAll(this.procuraTokens(linha));
+    	ArrayList<String> tokens = new ArrayList<String>();
+    	if(linha.matches(declaracaoDeVariavel)){
+    		tokens.addAll(this.procuraTokens(linha, Interpretador.espacoEmBranco + "|varDouble|;"));
+    		if(this.declararVariavel(tokens.get(0)) == null){
+    			System.out.println("Erro na linha @@@ " + (i+1) + " @@@ Variavel ### " + tokens.get(0) + " ### já declarada");
+    			System.exit(0);
+    		}		
+    	}else if(linha.matches(declaracaoDeVariavelComAtribuicao)){
+    		
+    		/*
+    		 * 
+    		 * 
+    		 * 
+    		 * parei aqui, fazer agora declaracao de variavel com tribuicao
+    		 * 
+    		 * 
+    		 * 
+    		 * 
+    		 */
+    		
+    		
+    		
+    		
+    		System.out.println("declaracao de variavel com atribuicao");
+    	}else{
+    		System.out.println("erroooooooooooooo");
+    	}
     	//depuracao
-    	for(int i=0; i < tokens.size(); i++) System.out.println("'" + tokens.get(i) + "'");
-    	
-    	
-    	/*if(linha.matches(declaracaoDeVariavel)){ // caso seja somente a declaração da variavel
-    		if(!this.declararVariavel(tokens))
-    			System.out.println("Erro na linha, variavel ja existe");
-    	}*/
+    	//for(int i=0; i < tokens.size(); i++) System.out.println("'" + tokens.get(i) + "'");
 	}    
     
-    // metodo que tenta fazer a declaracao de uma variavel, retorna true se sim, e false se nao conseguir declarar
-    public boolean declararVariavel(ArrayList<String> tokens){
-    	if(tokens.size() > 1){
-			for(int indice = 0;indice < tokens.size(); indice++){
-				//se entrar neste if, significa que é o nome da variavel
-				//System.out.println(indice + "'"+tokens[indice]+"'"+  "\ttamanho" + tokens[indice].length());
-				if(!tokens.get(indice).equals("double") && !tokens.get(indice).equals(";")){
-					if(this.verificaSeExisteVariavel(tokens.get(indice))){
-						System.out.println("variavel ja existe");
-						return false;
-					}else{
-						System.out.println("variavel ### " + tokens.get(indice) + " ### ainda nao existe, pode declarar");
-						Variavel newVar = new Variavel();
-						newVar.setNome(tokens.get(indice));
-						variavel.add(newVar);
-						return true;
-					}
-					//System.out.println(tokens[indice]);	// imprime o nome da variavel
-				}
-			}	
-		}
-		return false;
+    public boolean declararVariavelComAtribuicao(String nome, double valor){
+    	if(declararVariavel(nome) != null){
+    		Variavel newVar = declararVariavel(nome);
+    		newVar.setValor(valor);
+    		return true;
+    	}else{
+    		System.out.println("variavel ja existe");
+    		return false;
+    	}
     }
-        
+    
+    
+    
+    //chama metodo que vai tentar declarar variavel sem atribuicao
+    public Variavel declararVariavel(String nome){
+    	if(!verificaSeExisteVariavel(nome)){
+    		//System.out.println("declarando variavel..." + nome);
+    		Variavel newVar = new Variavel();
+    		newVar.setNome(nome);
+    		variavel.add(newVar);
+    		return newVar;
+    	}
+    	System.out.println("variavel ja existe");
+    	return null;
+    }    
+ 
     // metodo que separa a linha por tokens
-     public ArrayList<String> procuraTokens(String linha){
+     public ArrayList<String> procuraTokens(String linha, String separadores){
     	//System.out.println("procurando tokens na linha...\n------>>>>>>>>>");
     	System.out.println(linha);
     	String[] tokens;
-    	tokens = linha.split(Interpretador.espacoEmBranco); // separa a linha por espacos em branco
+    	tokens = linha.split(separadores); // separa a linha por espacos em branco
     	//depuracao
     	//for(int cont = 0; cont < tokens.length; cont++){ System.out.println("'" + tokens[cont] + "'" +  " tamanho " + tokens[cont].length()); }
     	ArrayList<String>subTokens = new ArrayList<String>();
-    	if(tokens.length > 1) 
+    	if(tokens.length > 1)
     		// percorre o vetor de tokens, procurando por espacos em branco novamente
     		for(int indice = 0; indice < tokens.length; indice++)
     			if(tokens[indice].length() > 0){
-    				System.out.println("aqui");
-    				//subTokens.add(tokens[indice]);
-    				if(tokens[indice].equals("\\w{1,}[\\;\\s{0,})$")){
-    					//System.out.println("termina com ;");
-    				}
+    				//System.out.println("aqui");
+    				subTokens.add(tokens[indice]);
     			}
-    				//subTokens.add(tokens[indice]); 
+    			//subTokens.add(tokens[indice]); 
     		//depuracao
     		//for(int i = 0; i < subTokens.size(); i++){ System.out.println("'" + subTokens.get(i) + "'" + " tamanho" + subTokens.get(i).length());}
        	return subTokens;
@@ -104,7 +121,7 @@ class Interpretador {
     public boolean verificaSeExisteVariavel(String var){
 		for(int cont = 0; cont < this.variavel.size(); cont++){
 			if(var.equals(this.variavel.get(cont).getNome())){
-				System.out.println("variavel ja existe");
+				//System.out.println("variavel ja existe");
 				return true;
 			}
 		}
