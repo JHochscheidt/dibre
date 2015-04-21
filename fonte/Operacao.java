b162e3a32bedf0d0689485137d2998a08e7beb27
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-20 23:01:28 -0300

    classe que faz as operacoes de adicao, subtracao, multiplicacao e divisao

--- fonte/Operacao.java
+++ fonte/Operacao.java
@@ -1,13 +1,16 @@
 class Operacao{
-	
-	
-	Variavel operando[];
-	String operador[];
-	
-	
-	
-	
-	
-	
-	
+		
+	public static double operacao(double operando1, double operando2, String operacao){
+		if(operacao.equals("SUBTRAI"))
+			return operando1 - operando2;
+		else if(operacao.equals("SOMA"))
+			return operando1 + operando2;
+		else if(operacao.equals("MULTIPLICA"))
+			return operando1 * operando2;
+		else if(operacao.equals("DIVIDE"))
+			return operando1 / operando2;
+		else
+			return 0;
+		
+	}
 }
