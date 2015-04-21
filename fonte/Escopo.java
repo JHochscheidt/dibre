366398eee4323bef81266f95d7161f9afc36f9ed
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2015-04-21 16:02:06 -0300

    classe que guarda o inicio e o fim de um escopo, guarda tambem um nome, embora nao faça sentido

--- fonte/Escopo.java
+++ fonte/Escopo.java
@@ -1,22 +1,34 @@
 class Escopo{
 
-	private short inicio;
-	private short fim;
+	private int inicio;
+	private int fim;
+	private String nome;
 
-
-	public void setInicio(short i){
+	
+	public Escopo(String n, int i){
+		this.nome = n;
 		this.inicio = i;
 	}
-	public short getInicio(){
+	
+	public void setInicio(int i){
+		this.inicio = i;
+	}
+	public int getInicio(){
 		return this.inicio;
 	}
 	
-	public void setFim(short f){
+	public void setFim(int f){
 		this.fim = f;
 	}
-	public short getFim(){
+	public int getFim(){
 		return this.fim;
 	}
 	
+	public void setNome(String n){
+		this.nome = n;
+	}
+	public String getNome(){
+		return this.nome;
+	}
 	
 }
